eab8b184bb7d61411332f923a16b039e61d82941
kmety <kmety9876@gmail.com>
2019-11-04 14:09:37 +0200

    first commit

---
+++ src/main/java/ua/mate/academy/ukrainedeputies/UkraineDeputiesApplication.java
@@ -0,0 +1,13 @@
+package ua.mate.academy.ukrainedeputies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkraineDeputiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UkraineDeputiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/mate/academy/ukrainedeputies/UkraineDeputiesApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.mate.academy.ukrainedeputies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkraineDeputiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
